fbd50300b258910e91adf8f31b6d6b62ca3e3121
SonyNeto <sonyneto@gmail.com>
2026-06-02 11:01:33 -0300

    refactor: use Base UI Avatar

--- src/components/commons/Avatar.tsx
+++ src/components/commons/Avatar.tsx
@@ -1,5 +1,5 @@
-import * as AvatarPrimitive from '@radix-ui/react-avatar';
+import * as AvatarPrimitive from '@base-ui/react/avatar';
 
-export const Avatar = AvatarPrimitive.Root;
-export const AvatarImage = AvatarPrimitive.Image;
-export const AvatarFallback = AvatarPrimitive.Fallback;
+export const Avatar = AvatarPrimitive.Avatar.Root;
+export const AvatarImage = AvatarPrimitive.Avatar.Image;
+export const AvatarFallback = AvatarPrimitive.Avatar.Fallback;
