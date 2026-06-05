724ee3d63fdf18376a7e7f79345201c249147a48
SonyNeto <sonyneto@gmail.com>
2026-06-05 13:59:34 -0300

    refactor: use Base UI Tooltip

--- src/components/commons/Tooltip.tsx
+++ src/components/commons/Tooltip.tsx
@@ -1,24 +1,30 @@
-import * as PrimitiveTooltip from '@radix-ui/react-tooltip';
-import type { ComponentProps, FC } from 'react';
+import { Tooltip as PrimitiveTooltip } from '@base-ui/react/tooltip';
+import type { FC } from 'react';
 import { cn } from '../../utils/functions';
+import type React from 'react';
 
+export const TooltipProvider = PrimitiveTooltip.Provider;
 export const Tooltip = PrimitiveTooltip.Root;
 export const TooltipTrigger = PrimitiveTooltip.Trigger;
-export const TooltipContent: FC<ComponentProps<typeof PrimitiveTooltip.Content>> = ({
-  className,
-  children,
-  ...props
-}) => (
+
+type TooltipProps = React.ComponentPropsWithoutRef<typeof PrimitiveTooltip.Popup>;
+type TooltipPositionerProps = React.ComponentPropsWithoutRef<typeof PrimitiveTooltip.Positioner>;
+type TooltipContentProps = TooltipProps &
+  Omit<TooltipPositionerProps, keyof TooltipProps | 'className' | 'style' | 'render'>; // Positioner deals with geomtry, while Popup is the actual content.
+
+export const TooltipContent: FC<TooltipContentProps> = ({ className, children, ...props }) => (
   <PrimitiveTooltip.Portal>
-    <PrimitiveTooltip.Content
-      className={cn(
-        className,
-        'rounded bg-zinc-800 px-[15px] py-2.5 text-[15px] leading-none text-white shadow-lg select-none',
-      )}
-      {...props}
-    >
-      {children}
-      <PrimitiveTooltip.Arrow className="fill-zinc-800" />
-    </PrimitiveTooltip.Content>
+    <PrimitiveTooltip.Positioner>
+      <PrimitiveTooltip.Popup
+        className={cn(
+          className,
+          'rounded bg-zinc-800 px-[15px] py-2.5 text-[15px] leading-none text-white shadow-lg select-none',
+        )}
+        {...props}
+        render={children as React.ReactElement}
+      >
+        <PrimitiveTooltip.Arrow className="fill-zinc-800" />
+      </PrimitiveTooltip.Popup>
+    </PrimitiveTooltip.Positioner>
   </PrimitiveTooltip.Portal>
 );
